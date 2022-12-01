a82cef46189044160171524302b0678f5f344ac5
Herbert-Santos <hebertesantos123@gmail.com>
2022-12-01 00:29:02 -0300

    Subindo os primeiros passos da blend

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+const app = express();
+
+app.use(express.json());
+
+/**
+ * vamos usar esta variavel para armazenar qualquer dados, para usar de teste.
+ * Ao atualizar o codigo, essa variavel que continha valores, sera reiniciada sem nenhum valor. Depois usaremos
+ * um banco de dados para melhor evolucao. jamais use essa variavel em producao.
+ * 👇🏻
+ */
+
+const database = [];
+
+app.get('/Home', (request, response) => {
+    const {title} = request.query;
+    const results = title
+
+    ? database.filter(use => use.title.includes(title))
+    : database;
+
+    return response.json(results);
+});
+
+app.post('/Home', (request, response) => {
+    const {title, owner, email, pedido} = request.body;
+    const user = {id: uuid(), title, owner, email, pedido};
+    database.push(user);
+
+    console.log(user);
+    return response.json(user);
+});
+
+app.put('/Home/:id', (request, response) => {
+    const {id} = request.params;
+    const {title, owner, email, pedido} = request.body;
+    const userUnic = database.findIndex(user => user.id == id);
+
+    if(userUnic < 0){
+        return response.status(400).json({error:'User not found.'})
+    }
+
+    const user = {
+        id,
+        title,
+        owner,
+        email,
+        pedido,
+    }
+
+    console.log('um usuario foi atualizado ', user);
+    return response.json(user);
+});
+
+app.delete('/Home/:id', (request, response) => {
+    const {id} = request.params;
+    const userUnic = database.findIndex(user => user.id == id);
+
+    if(userUnic < 0){
+        return response.status(400).json({error:'User not found.'})
+    }
+
+    database.splice(userUnic, 1);
+    return response.status(204).send();
+});
+
+app.listen(3333, () => {
+    console.log('⛳️ Back-end started!');
+});
